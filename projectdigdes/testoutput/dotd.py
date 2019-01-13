0b7a66c7f75af4d875e37f03df47fea03fa93575
djino97 <38541098+djino97@users.noreply.github.com>
2019-01-13 17:28:03 +0300

    Add files via upload

--- projectdigdes/testoutput/dotd.py
+++ projectdigdes/testoutput/dotd.py
@@ -5,15 +5,15 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import (QToolTip, QPushButton, QLabel)
 from PyQt5.QtGui import QFont
-from PyQt5 import QtCore
 from interface import Example
 import os
 import sys
 from buildgraph import get_route, get__points, \
-    get_pos_agent, math_position, make_dot, lst_optimal, update_graph, put_truck, update_point_graph
-from project.dijkstra import dijkstra
+    get_pos_agent, math_position, make_dot, update_graph, \
+    put_truck, update_point_graph, get_date_supply, update_graphic, main
 from project.math_route import MathRoute
 from search_route import list_routs
+import datetime
 
 # Read a section in an .ini file and return a dictionnary
 #  @param s_ini_file Path to the .ini file to read
@@ -49,26 +49,34 @@ class MyLabel(QtWidgets.QLabel):
         self.num_step = 0
         self.pos_x_point = 0
         self.pos_y_point = 0
-        self.get_route = True
+        self.ride_truck = False
         self.step = False
         self.pos_x_next_point = 0
         self.pos_y_next_point = 0
         self.next_point = []
         self.supply = []
+        self.position_truck = []
         self.consumption = []
         self. point = []
+        self.add_truck = []
+        self.count_agent_route = 1
         self.all_route = []
-        self.count_route = 0
         self.search_route = True
+        self.count_route = 0
+        self.date_step, self.date_count_departure, self.date_count_arrival = get_date_supply()
 
         self.label_route = QLabel("Mаршруты:", self)
-        self.label_route.move(280, 0)
+        self.label_route.move(295, 0)
+
+        self.btn_step = QPushButton(self)
+        self.btn_step.resize(self.btn_step.sizeHint())
+        self.btn_step.move(100, 0)
+        self.btn_step.clicked.connect(self.step_date)
         
 # Daemon that run the GraphViz dot tool and display the image in a QLabel window
     def daemon(self):
         from subprocess import call
         from PyQt5 import QtGui
-        import datetime
 
         dot = "pydot.dot"
 
@@ -85,15 +93,11 @@ class MyLabel(QtWidgets.QLabel):
             self.update_route()
             self.search_route = False
 
-        btn_step = QPushButton('Шаг', self)
-        btn_step.resize(btn_step.sizeHint())
-        btn_step.move(100, 0)
-        btn_step.clicked.connect(self.step_date)
-
         btn_math_route = QPushButton('Расчет маршрута', self)
         btn_math_route.resize(btn_math_route.sizeHint())
-        btn_math_route.move(170, 0)
+        btn_math_route.move(189, 0)
         btn_math_route.clicked.connect(self.message_box)
+
         if self.mTime != os.path.getmtime(dot):
             # Run DOT
             t_run_dot = "{0} -T{1} \"{2}\" -o \"{2}.{1}\"".format(self.dDOT["exe"], self.dDOT["format"], dot)
@@ -108,9 +112,20 @@ class MyLabel(QtWidgets.QLabel):
             self.mTime = os.path.getmtime(dot)
 
     def update_route(self):
-        str_route = list_routs()
+        str_route, check = list_routs()
         self.label_route.setText(str_route)
         self.label_route.adjustSize()
+        self.date_step, self.date_count_departure, self.date_count_arrival = get_date_supply()
+        if self.date_step:
+            self.btn_step.setText('Шаг %s' % self.date_step.strftime('%d-%m-%Y'))
+        else:
+            self.btn_step.setText('Шаг  не задан')
+        self.btn_step.adjustSize()
+        self.count_agent_route = 1
+        if check != 0:
+            update_graphic()
+        else:
+            main()
 
     def open_win(self):
         if not self.secondWin:
@@ -131,21 +146,26 @@ class MyLabel(QtWidgets.QLabel):
         :return:if truck passes one agent, then return "True",
         else "False"
         """
-        if self.step is False:
-            self.all_route = get_route()
+        if not self.ride_truck and self.date_count_arrival != self.date_count_departure:
+            self.all_route = get_route(self.date_count_departure, self.count_agent_route)
             self.step = True
             self.all_route = self.all_route[self.count_route:]
-            self.count_route = self.count_route + 1
+            self.count_route += 1
             for route in self.all_route:
-                    agent_points, self.supply, self.consumption, self.next_point, self.point = get__points(route)
-                    for agent in agent_points:
-                        self.pos_x_point, self.pos_y_point, self.pos_x_next_point, \
-                         self.pos_y_next_point = get_pos_agent(agent)
-                        return
+                agent_points, self.supply, self.consumption, self.next_point, self.point = get__points(route)
+                for agent in agent_points:
+                    self.pos_x_point, self.pos_y_point, self.pos_x_next_point, \
+                       self.pos_y_next_point = get_pos_agent(agent)
+                    self.ride_truck = True
+                    return
             else:
-                self.step = False
                 self.count_route = 0
-                return 
+                self.count_agent_route += 1
+                self.step = False
+                self.date_count_departure = self.date_count_departure + datetime.timedelta(days=1)
+                self.btn_step.setText('Шаг %s' % self.date_count_departure.strftime('%d-%m-%Y'))
+                self.btn_step.adjustSize()
+                return
         else:
             # truck will not reach the end point, then coordinate calculations are performed
             if round(self.pos_x_point) != self.pos_x_next_point or round(self.pos_y_point) != self.pos_y_next_point:
@@ -153,20 +173,20 @@ class MyLabel(QtWidgets.QLabel):
                                                                               self.pos_x_next_point,
                                                                               self.pos_y_next_point)
                 dot_step = make_dot()
-                optimal = dijkstra(self.next_point, self.supply, self.consumption)
-                lst_pars = lst_optimal(optimal)
-                update_point_graph(self.point, dot_step)
+                update_point_graph(self.point, dot_step, False, ride_truck=self.add_truck,
+                                   img_truck=img_truck, supply=self.supply)
                 put_truck(dot_step, self.pos_x_point, self.pos_y_point, img_truck)
-                update_graph(lst_pars, self.next_point, dot_step)
+                update_graph(self.next_point, dot_step)
                 walk = os.getcwd()
                 write = open('%s/pydot.dot' % walk, 'w', encoding="UTF-8")
                 write.write(dot_step.source)
                 write.close()
                 return
-
             else:
-                self.step = False
-                return
+                self.add_truck = []
+                self.add_truck.append(self.pos_x_point)
+                self.add_truck.append(self.pos_y_point)
+                self.ride_truck = False
 
 
 # Main program
